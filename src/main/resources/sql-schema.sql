d4016576914214b6f4b7aa25507e8f3e526fd1d0
trsif <pepefava1@gmail.com>
2021-06-01 04:01:19 -0400

    no real change

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -10,3 +10,17 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+CREATE TABLE IF NOT EXISTS `items` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(40) DEFAULT NULL,
+    `value` double default 0,
+    PRIMARY KEY (`id`)
+);
+CREATE TABLE IF NOT EXISTS `orders` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customer_id` int not null,
+    `item_id`int not null,
+    FOREIGN KEY(`customer_id`) REFERENCES `customers`(`id`),
+	FOREIGN KEY(`item_id`) REFERENCES `items`(`id`),
+    PRIMARY KEY (`id`)
+);
